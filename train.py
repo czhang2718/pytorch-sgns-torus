90fa7a268a48441dcbf973e8d2f7dd5303bb6cd7
Claire Zhang <claire@worker-4.soperator-worker-svc.soperator.svc.cluster.local>
2025-11-10 00:32:52 +0000

    wandb + gitignore data and wandb folders

--- train.py
+++ train.py
@@ -12,6 +12,13 @@ from torch.optim import Adam
 from torch.utils.data import Dataset, DataLoader
 from model import Word2Vec, SGNS
 
+try:
+    import wandb
+    WANDB_AVAILABLE = True
+except ImportError:
+    WANDB_AVAILABLE = False
+    print("Warning: wandb not available. Install with: pip install wandb")
+
 
 def parse_args():
     parser = argparse.ArgumentParser()
@@ -28,6 +35,11 @@ def parse_args():
     parser.add_argument('--weights', action='store_true', help="use weights for negative sampling")
     parser.add_argument('--cuda', action='store_true', help="use CUDA")
     parser.add_argument('--torus', action='store_true', default=False, help="train toric embeddings")
+    parser.add_argument('--wandb', action='store_true', help="log to Weights & Biases")
+    parser.add_argument('--wandb_name', type=str, default='pytorch-sgns-torus', help="wandb run name")
+    parser.add_argument('--wandb_project', type=str, default='pytorch-sgns-torus', help="wandb project name")
+    parser.add_argument('--wandb_entity', type=str, default=None, help="wandb entity/team name")
+    parser.add_argument('--wandb_log_interval', type=int, default=1, help="log to wandb every N batches")
     return parser.parse_args()
 
 
@@ -74,7 +86,28 @@ def train(args):
     if os.path.isfile(optimpath) and args.conti:
         optim.load_state_dict(t.load(optimpath))
     
+    # Initialize wandb
+    if args.wandb and WANDB_AVAILABLE:
+        wandb.init(
+            project=args.wandb_project,
+            entity=args.wandb_entity,
+            name=args.wandb_name,
+            config={
+                'vocab_size': vocab_size,
+                'embedding_dim': args.e_dim,
+                'n_negs': args.n_negs,
+                'epochs': args.epoch,
+                'batch_size': args.mb,
+                'learning_rate': args.lr,
+                'subsample_threshold': args.ss_t,
+                'use_weights': args.weights,
+                'torus': args.torus,
+                'cuda': args.cuda,
+            }
+        )
+    
     best_loss = float('inf')
+    tokens_seen = 0
     
     for epoch in range(1, args.epoch + 1):
         dataset = PermutedSubsampledCorpus(os.path.join(args.data_dir, 'train.dat'))
@@ -84,7 +117,7 @@ def train(args):
         pbar = tqdm(dataloader)
         pbar.set_description("[Epoch {}]".format(epoch))
         
-        for iword, owords in pbar:
+        for batch_idx, (iword, owords) in enumerate(pbar, 1):
             loss = sgns(iword, owords)
             optim.zero_grad()
             loss.backward()
@@ -93,22 +126,41 @@ def train(args):
             loss_val = loss.item()
             epoch_losses.append(loss_val)
             
+            # Count tokens: batch_size * context_size
+            batch_size = iword.size(0)
+            context_size = owords.size(1)
+            tokens_seen += batch_size * context_size
+            
             # Check for NaN or Inf (bug detection)
             if not np.isfinite(loss_val):
-                print(f"\nWARNING: Non-finite loss detected at epoch {epoch}: {loss_val}")
+                print(f"\nWARNING: Non-finite loss detected at epoch {epoch}, batch {batch_idx}: {loss_val}")
+                if args.wandb and WANDB_AVAILABLE:
+                    wandb.alert(title="NaN/Inf Loss Detected", text=f"Epoch {epoch}, Batch {batch_idx}, Loss: {loss_val}")
                 break
             
             pbar.set_postfix(loss=loss_val)
+            
+            # Log to wandb at specified intervals
+            if args.wandb and WANDB_AVAILABLE and (batch_idx % args.wandb_log_interval == 0):
+                wandb.log({'loss': loss_val, 'epoch': epoch}, step=tokens_seen)
         
         # Log at end of epoch
         epoch_avg_loss = np.mean(epoch_losses)
         if epoch_avg_loss < best_loss:
             best_loss = epoch_avg_loss
         print(f"[Epoch {epoch}/{args.epoch}] Average Loss: {epoch_avg_loss:.4f}, Best Loss: {best_loss:.4f}")
+        
+        # Log epoch average to wandb
+        if args.wandb and WANDB_AVAILABLE:
+            wandb.log({'loss': epoch_avg_loss, 'epoch': epoch}, step=tokens_seen)
     idx2vec = model.ivectors.weight.data.cpu().numpy()
     pickle.dump(idx2vec, open(os.path.join(args.data_dir, 'idx2vec.dat'), 'wb'))
     t.save(sgns.state_dict(), os.path.join(args.save_dir, '{}.pt'.format(args.name)))
     t.save(optim.state_dict(), os.path.join(args.save_dir, '{}.optim.pt'.format(args.name)))
+    
+    # Finish wandb run
+    if args.wandb and WANDB_AVAILABLE:
+        wandb.finish()
 
 
 if __name__ == '__main__':
